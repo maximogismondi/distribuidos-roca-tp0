47d5ea158ef389b3cfbb11e29a7f19cf239f660e
MaximoGismondi <maximogismondi@gmail.com>
2025-03-22 20:21:32 -0300

    Fix: Network prefix and proper encoding for grep

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,12 +2,12 @@
 echo "This script will validate the echo server"
 
 TEST_TEXT="Hello, World!"
-REGEX="^$TEST_TEXT$"
-SCRIPT = "echo $TEST_TEXT | nc server 12345 | grep -q $REGEX"
+REGEX="^$TEST_TEXT\$"
 
 # ejecutar un alpine básico en la red testing_net
 
-docker run --rm --network testing_net alpine sh -c "$SCRIPT"
+docker run --rm --network tp0_testing_net alpine sh -c "\
+    echo \"$TEST_TEXT\" | nc server 12345 | grep -q \"$REGEX\""
 
 # ahora en funcion del resultado imprimir un mensaje adecuado
 

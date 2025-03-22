0f26a485d1c025c674de3bbc3148e376f21cfd75
MaximoGismondi <maximogismondi@gmail.com>
2025-03-22 20:12:23 -0300

    Fix: Now the script just wakes up the tester, not the server as well

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
+echo "This script will validate the echo server"
 
-# Generar el archivo docker-compose
-DOCKER_COMPOSE_FILE="docker-compose-test.yaml"
-python3 docker_compose_server_validator.py $DOCKER_COMPOSE_FILE
+TEST_TEXT="Hello, World!"
+REGEX="^$TEST_TEXT$"
+SCRIPT = "echo $TEST_TEXT | nc server 12345 | grep -q $REGEX"
 
-# Build the images
-docker compose -f $DOCKER_COMPOSE_FILE build
+# ejecutar un alpine básico en la red testing_net
 
-# Levantar solo el server en segundo plano
-docker compose -f $DOCKER_COMPOSE_FILE up -d server
+docker run --rm --network testing_net alpine sh -c "$SCRIPT"
 
-# Ejecutar el test desde el cliente y capturar el resultado real
-docker compose -f $DOCKER_COMPOSE_FILE run --rm test_client
-RESULT=$?
+# ahora en funcion del resultado imprimir un mensaje adecuado
 
-# Verificar el resultado y mostrar el mensaje correspondiente
-if [ $RESULT -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
 fi
-
-# Bajar los servicios y limpiar
-docker compose -f $DOCKER_COMPOSE_FILE down
-
-# Eliminar el archivo docker-compose
-rm $DOCKER_COMPOSE_FILE

e1f67b6208d95d31e6e19c6929092a95caf07af7
MaximoGismondi <maximogismondi@gmail.com>
2025-03-22 19:05:42 -0300

    Refactor: Change python script file name from - to _

--- generar-compose.sh
+++ generar-compose.sh
@@ -3,4 +3,4 @@ echo "This script will generate a docker-compose file for you"
 echo "The output file name will be $1"
 echo "The number of clients will be $2"
 
-python3 docker-compose-generator.py $1 $2
+python3 docker_compose_generator.py $1 $2
